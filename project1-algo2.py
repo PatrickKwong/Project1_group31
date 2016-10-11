56a4fc7ae806d1293913a7fe985f5eedae224e1e
Patrick Kwong <patrick.kwong@gmail.com>
2016-10-10 22:32:50 -0700

    Added Algorithm #2
    Added Algorithm #2 and the other test file MSS_Problems.txt

---
+++ project1-algo2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# Patrick Kwong
+# CS325 Analysis of Algorithms - Project 1 Group 31
+# Algorithm 2: Better Enumeration
+# October 10, 2016
+
+# O(n^2) Better Enumeration - iteration for max subarray.
+# Source: Based on Prof. Borradaile's Designing Poly-Time Algorithms lecture
+def Algorithm2(array):
+		maxSum = 0
+		maxArrayIndexLow = 0
+		maxArrayIndexHigh = 0
+
+		for i in range(0, len(array)):
+			thisSum = 0
+			for j in range(i, len(array)):
+				thisSum = thisSum + array[j]
+				
+				if (thisSum > maxSum):
+					maxSum = thisSum
+					maxArrayIndexLow = i
+					maxArrayIndexHigh = j
+
+		maxArray = []
+		maxArray = array[maxArrayIndexLow:maxArrayIndexHigh+1]
+		print array
+		print maxArray
+		print maxSum
+
+
+#Here is the "main" function so far it just reads every line from a file and sends it to algorithm
+with open('MSS_TestProblems.txt','r') as f:
+	#parse each line in test file and store in array to send to the different functions
+
+	for line in f:
+			line = line.replace("[","")
+			line = line.replace("]","")
+			line = line.replace(" ","")
+			array = [int(x) for x in line.split(',') if x not in '\n']
+
+			Algorithm2(array) #call algorithm on each line in test file
