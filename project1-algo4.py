29d9d28e2a27d10f43719f15d256d995ab5deeab
Nicholas Vrontakis <niktak11@yahoo.com>
2016-10-15 11:55:44 -0700

    added runtime data collection
    added .gitignore for the runtime results file

--- project1-algo4.py
+++ project1-algo4.py
@@ -6,41 +6,48 @@
 
 # O(n) Linear-time - iteration for max subarray.
 # Source: Based on Prof. Borradaile's Designing Poly-Time Algorithms lecture and the provided Algorithm 4 Pseudocode.pdf
+
+import time
+
 def Algorithm4(array):
-	n = len(array)
-	maxSum = -10000
-	endingHereSum = -10000
+    n = len(array)
+    maxSum = -10000
+    endingHereSum = -10000
 
-	for i in range(0, n):
-		endingHereHigh= i
+    for i in range(0, n):
+        endingHereHigh= i
 
-		if endingHereSum > 0:
-			endingHereSum = endingHereSum + array[i]
+        if endingHereSum > 0:
+            endingHereSum = endingHereSum + array[i]
 
-		else:
-			endingHereLow = i
-			endingHereSum = array[i]
+        else:
+            endingHereLow = i
+            endingHereSum = array[i]
 
-		if endingHereSum > maxSum:
-			maxSum = endingHereSum
-			maxArrayIndexLow = endingHereLow
-			maxArrayIndexHigh = endingHereHigh
+        if endingHereSum > maxSum:
+            maxSum = endingHereSum
+            maxArrayIndexLow = endingHereLow
+            maxArrayIndexHigh = endingHereHigh
 
-	maxArray = []
-	maxArray = array[maxArrayIndexLow:maxArrayIndexHigh+1]
-	print array
-	print maxArray
-	print maxSum
+    maxArray = []
+    maxArray = array[maxArrayIndexLow:maxArrayIndexHigh+1]
+    print array
+    print maxArray
+    print maxSum
 
 
 #Here is the "main" function so far it just reads every line from a file and sends it to algorithm
 with open('MSS_TestProblems.txt','r') as f:
-	#parse each line in test file and store in array to send to the different functions
+    #parse each line in test file and store in array to send to the different functions
 
-	for line in f:
-			line = line.replace("[","")
-			line = line.replace("]","")
-			line = line.replace(" ","")
-			array = [int(x) for x in line.split(',') if x not in '\n']
+    for line in f:
+        line = line.replace("[","")
+        line = line.replace("]","")
+        line = line.replace(" ","")
+        array = [int(x) for x in line.split(',') if x not in '\n']
 
-			Algorithm4(array) #call algorithm on each line in test file
+        cur_time = time.clock()
+        Algorithm4(array) #call algorithm on each line in test file
+        elapsed_time = time.clock() - cur_time
+        print str(num) + ", " + str(round(elapsed_time, 7))
+        num += 1
